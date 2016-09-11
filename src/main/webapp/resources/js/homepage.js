cdb66cbda5d63c0923797112048249f5e2fa5dd0
cbh920 <497285104@qq.com>
2016-09-11 16:39:59 +0800

    hh

--- src/main/webapp/resources/js/homepage.js
+++ src/main/webapp/resources/js/homepage.js
@@ -44,6 +44,11 @@ $(document).ready(function(){
 		$(".menu_right").html("<iframe src='order/sent' width='' height='' frameborder='0'></iframe>");
 	});
 	
+	$(lis[2]).live("click",function(){
+		$(".menu_right").html("<iframe src='order/return' width='' height='' frameborder='0'></iframe>");
+	});
+	
+	
 });
 //会员管理页面跳转
 $(document).ready(function(){
